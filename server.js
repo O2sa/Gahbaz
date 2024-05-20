b318550b08a5df766743ac0c102a61b4015f8fc7
o2sa <o2sama1@outlook.sa>
2024-05-20 09:13:41 +0300

    add the api

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ if (process.env.NODE_ENV === "development") {
 // );
 
 
-app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static(path.resolve(__dirname, "./client/dist")));
 app.use(cookieParser());
 app.use(express.json());
 app.use(helmet());
@@ -91,7 +91,7 @@ app.use("/api/v1/semester-templates", semesterTemplateRoute);
 
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
 });
 
 app.use("*", (req, res) => {
